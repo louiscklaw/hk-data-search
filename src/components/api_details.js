f6b5f5e332dbfaf27873703dcdcedbc536a5c665
louiscklaw <louiscklaw@gmail.com>
2020-05-26 17:40:22 +0800

    update with json-stable-stringify,

--- src/components/api_details.js
+++ src/components/api_details.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Link} from 'gatsby'
 
+import TableSample from './table_sample'
 import HighlightPre from './highlight_pre'
+import stringify from 'json-stable-stringify'
 
 import {GlobalContext} from '../context/GlobalContext'
 
 function ApiDetails(props){
   let {getApiByName} = React.useContext(GlobalContext)
+  let api_json = getApiByName(props.api_name)
 
   return(
 
@@ -18,9 +21,14 @@ function ApiDetails(props){
 
       <Link to="/zh">back</Link>
 
+      <div style={{'margin':'auto','text-align':'center'}}>
+        <TableSample api_json={api_json}/>
+      </div>
+
+      <h3>raw json:</h3>
       <HighlightPre
         language="json"
-        content={JSON.stringify(getApiByName(props.api_name),null,2)}
+        content={stringify(api_json,{space: 1})}
         />
 
     </div>
